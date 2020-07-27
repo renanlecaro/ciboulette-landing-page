9312fbce8d2476df0c8b69e463f7f64f469c31fc
Renan LE CARO <renan.lecaro@gmail.com>
2020-07-27 20:57:08 +1000

    Switched articles order to have the notification make more or less sense in app

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Nouveau concept pour l'expérience de vos clients","url":"https://ciboulette.net/blog/2020-07-27","date":"2020-07-27T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Nouveaux articles du blog directement dans l'appli","url":"https://ciboulette.net/blog/2020-07-27","date":"2020-07-27T00:00:00.000Z"});
   
