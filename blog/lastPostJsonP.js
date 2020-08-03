86a696a43df1e4a9cec7e6e72c69a7ec9381c97c
Renan LE CARO <renan.lecaro@gmail.com>
2020-08-03 19:21:35 +1000

    New blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Petites corrections, et invitation à discuter de l'application","url":"https://ciboulette.net/blog/2020-07-30","date":"2020-07-30T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Produits en paniers uniquement","url":"https://ciboulette.net/blog/2020-08-03","date":"2020-08-03T00:00:00.000Z"});
   
