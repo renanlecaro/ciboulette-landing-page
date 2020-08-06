a21ea744469489ddde78a4709986ebe4a491bb19
Renan LE CARO <renan.lecaro@gmail.com>
2020-08-06 10:27:20 +1000

    Blog update

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Produits en paniers uniquement","url":"https://ciboulette.net/blog/2020-08-03","date":"2020-08-03T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Future éditeur de produit","url":"https://ciboulette.net/blog/2020-08-05","date":"2020-08-05T00:00:00.000Z"});
   
