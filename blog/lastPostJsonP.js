d0a9b53f9a3bc921debda9b6bc5b4d412383850f
Renan LE CARO <renan.lecaro@gmail.com>
2020-08-28 16:12:42 +1000

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Editeur de RDV v2 ","url":"https://ciboulette.net/blog/2020-08-12","date":"2020-08-12T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Une nouvelle version de l'appli sera déployée lundi","url":"https://ciboulette.net/blog/2020-08-28","date":"2020-08-28T00:00:00.000Z"});
   
