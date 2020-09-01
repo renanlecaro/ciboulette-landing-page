178da7dd414de6f27b71a5e5c403a002a8fedf35
Renan LE CARO <renan.lecaro@gmail.com>
2020-09-02 07:48:05 +1000

    Reopened signups

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Paiement en ligne ? ","url":"https://ciboulette.net/blog/2020-08-31","date":"2020-08-31T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Réouverture des inscriptions","url":"https://ciboulette.net/blog/2020-09-01","date":"2020-09-01T00:00:00.000Z"});
   
