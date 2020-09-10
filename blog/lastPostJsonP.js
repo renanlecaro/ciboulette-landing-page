fdae230e7c455b9ecb2397cd6a7479bc8fa93049
Renan LE CARO <renan.lecaro@gmail.com>
2020-09-10 17:58:20 +1000

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Simplification de l'éditeur de textes","url":"https://ciboulette.net/blog/2020-09-08","date":"2020-09-08T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Les SMS étaient off pendant 2 jours","url":"https://ciboulette.net/blog/2020-09-10","date":"2020-09-10T00:00:00.000Z"});
   
