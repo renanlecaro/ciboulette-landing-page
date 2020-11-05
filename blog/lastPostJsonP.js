02590600404065503e0ca4659300751ffd1b54a5
Renan LE CARO <renan.lecaro@gmail.com>
2020-11-05 15:32:27 +1000

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Octobre improductif","url":"https://ciboulette.net/blog/2020-10-31","date":"2020-10-31T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Nouvelle organisation pour les paramètres","url":"https://ciboulette.net/blog/2020-11-05","date":"2020-11-05T00:00:00.000Z"});
   
