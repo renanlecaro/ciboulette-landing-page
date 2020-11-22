f0912ca3c01a9b83bfcfab9025fec8fc4e7f8b0e
Renan LE CARO <renan.lecaro@gmail.com>
2020-11-22 10:33:54 +1000

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Nouvelle fonction pour mieux vendre les courges","url":"https://ciboulette.net/blog/2020-11-13","date":"2020-11-13T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Filtre de rendez vous dans l'export des ventes","url":"https://ciboulette.net/blog/2020-11-22","date":"2020-11-22T00:00:00.000Z"});
   
