a8d75075bb2f8d0d002eb1d7b51c34c3a87b3891
Renan LE CARO <renan.lecaro@gmail.com>
2020-11-23 12:26:06 +1000

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Filtre de rendez vous dans l'export des ventes","url":"https://ciboulette.net/blog/2020-11-22","date":"2020-11-22T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Plus de details quand le bon de commande plante","url":"https://ciboulette.net/blog/2020-11-23","date":"2020-11-23T00:00:00.000Z"});
   
