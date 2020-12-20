527d5b84dc09f5e107ab087206612830dbf2a5db
Renan LE CARO <renan.lecaro@gmail.com>
2020-12-20 16:10:02 +1000

    Annonce du délai de déploiement de la prochaine version

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Importateur de produits et nouvelles","url":"https://ciboulette.net/blog/2020-12-07","date":"2020-12-07T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Changements à venir","url":"https://ciboulette.net/blog/2020-12-20","date":"2020-12-20T00:00:00.000Z"});
   
