41e96b4122e6c8261e1b8fefcfdadb7567415295
Renan LE CARO <renan.lecaro@gmail.com>
2021-01-02 18:36:58 +0100

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Changements à venir","url":"https://ciboulette.net/blog/2020-12-20","date":"2020-12-20T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Invitation à commander par SMS","url":"https://ciboulette.net/blog/2021-01-02","date":"2021-01-02T00:00:00.000Z"});
   
