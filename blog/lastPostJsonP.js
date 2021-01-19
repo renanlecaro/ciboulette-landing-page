7d07b1b26c3abe44cf495b6b6843cc4be537ea4c
Renan LE CARO <renan.lecaro@gmail.com>
2021-01-19 21:24:24 +0100

    blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Gestion des clients belges, bugfixes, admin ","url":"https://ciboulette.net/blog/2021-01-05","date":"2021-01-05T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Masquer des produits pour certains RDVs","url":"https://ciboulette.net/blog/2021-01-19","date":"2021-01-19T00:00:00.000Z"});
   
