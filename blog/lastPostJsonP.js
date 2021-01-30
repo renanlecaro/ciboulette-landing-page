b9e85543dd2a9046a06559196f518da6ae1ee83b
Renan LE CARO <renan.lecaro@gmail.com>
2021-01-30 17:42:34 +0100

    Script fix and added a blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Masquer des produits pour certains RDVs","url":"https://ciboulette.net/blog/2021-01-19","date":"2021-01-19T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Aidez ciboulette à se développer","url":"https://ciboulette.net/blog/2021-01-30","date":"2021-01-30T00:00:00.000Z"});
   
