5bc5fb71843b9332ed68ad7606f0269eee7995cc
Renan LE CARO <renan.lecaro@gmail.com>
2021-02-11 19:23:45 +0100

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Aidez ciboulette à se développer","url":"https://ciboulette.net/blog/2021-01-30","date":"2021-01-30T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Changements à venir (emails, formulaire de commande, gestion des stocks)","url":"https://ciboulette.net/blog/2021-02-11","date":"2021-02-11T00:00:00.000Z"});
   
