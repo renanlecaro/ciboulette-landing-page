5b7a64557bf7d7c90afe920d0d65ee8b1164da9e
Renan LE CARO <renan.lecaro@gmail.com>
2021-02-28 09:56:54 +0100

    blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Option \"Panier obligatoire\" et autres améliorations.","url":"https://ciboulette.net/blog/2021-02-23","date":"2021-02-23T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Prix aux 100g, faq sur le site","url":"https://ciboulette.net/blog/2021-02-28","date":"2021-02-28T00:00:00.000Z"});
   
