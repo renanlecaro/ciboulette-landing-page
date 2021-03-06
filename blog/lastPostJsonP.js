877dba5a340173e89896e26ec983ed381ab538d9
Renan LE CARO <renan.lecaro@gmail.com>
2021-03-06 14:03:27 +0100

    Blog post about auto recipes

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Prix aux 100g, faq sur le site","url":"https://ciboulette.net/blog/2021-02-28","date":"2021-02-28T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Recettes automatiques, frais de livraison","url":"https://ciboulette.net/blog/2021-03-06","date":"2021-03-06T00:00:00.000Z"});
   
