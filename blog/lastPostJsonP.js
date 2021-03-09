7fe7b6dfe6f5f828533399a6ad446f4f18095cae
Renan LE CARO <renan.lecaro@gmail.com>
2021-03-09 13:20:19 +0100

    Blog post warning about images in mails7

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Recettes automatiques, frais de livraison","url":"https://ciboulette.net/blog/2021-03-06","date":"2021-03-06T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Evitez les images dans les mails pour l'instant","url":"https://ciboulette.net/blog/2021-03-09","date":"2021-03-09T00:00:00.000Z"});
   
