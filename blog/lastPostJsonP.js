68cf5a3e62a41e4d3b0a44b5fffd02e1838382aa
Renan LE CARO <renan.lecaro@gmail.com>
2021-04-04 23:52:12 +0200

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Calculs plus rapides, sms plus fiables, tests automatisés, déploiement simplifiés ..","url":"https://ciboulette.net/blog/2021-03-30","date":"2021-03-30T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Compatibilité iphone, bugfixes","url":"https://ciboulette.net/blog/2021-04-04","date":"2021-04-04T00:00:00.000Z"});
   
