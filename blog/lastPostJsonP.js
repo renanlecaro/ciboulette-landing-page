6cec9b1b25832ff3b3b109ef650b8af675b98221
Renan LE CARO <renan.lecaro@gmail.com>
2021-04-18 11:51:43 +0200

    Post : rm vips ?

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Corrections de petits bugs","url":"https://ciboulette.net/blog/2021-04-06","date":"2021-04-06T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Retrait de la fonction \"clients VIP\"  ?","url":"https://ciboulette.net/blog/2021-04-18","date":"2021-04-18T00:00:00.000Z"});
   
