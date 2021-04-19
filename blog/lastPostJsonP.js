156c4075a103810ebf41ed0686ad1c3ec3533df6
Renan LE CARO <renan.lecaro@gmail.com>
2021-04-19 14:11:54 +0200

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Retrait de la fonction \"clients VIP\"  ?","url":"https://ciboulette.net/blog/2021-04-18","date":"2021-04-18T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Nouvelle option : réserver les stocks d'un panier","url":"https://ciboulette.net/blog/2021-04-19","date":"2021-04-19T00:00:00.000Z"});
   
