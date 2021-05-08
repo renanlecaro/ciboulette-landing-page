c92268a91a16f79763799a6cc8920eb44fa3d5d9
Renan LE CARO <renan.lecaro@gmail.com>
2021-05-08 17:46:12 +0200

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Nouvelle option : réserver les stocks d'un panier","url":"https://ciboulette.net/blog/2021-04-19","date":"2021-04-19T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Changements à venir","url":"https://ciboulette.net/blog/2021-05-08","date":"2021-05-08T00:00:00.000Z"});
   
