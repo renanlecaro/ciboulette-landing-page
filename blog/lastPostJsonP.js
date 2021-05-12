cde55bf1114ba556cc8d5ccd2b90e697b8e70234
Renan LE CARO <renan.lecaro@gmail.com>
2021-05-13 01:49:41 +0200

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Changements à venir","url":"https://ciboulette.net/blog/2021-05-08","date":"2021-05-08T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Déployé : plusieurs commandes par client","url":"https://ciboulette.net/blog/2021-05-12","date":"2021-05-12T00:00:00.000Z"});
   
