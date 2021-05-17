1910f51cf41989f98618334e4a9376c5e97fe7b3
Renan LE CARO <renan.lecaro@gmail.com>
2021-05-17 17:30:51 +0200

    Blog post : mass client edit

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Petits correctifs suite à la dernière livraison ","url":"https://ciboulette.net/blog/2021-05-15","date":"2021-05-15T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Nouvelle fonction : modification de clients en masse","url":"https://ciboulette.net/blog/2021-05-17","date":"2021-05-17T00:00:00.000Z"});
   
