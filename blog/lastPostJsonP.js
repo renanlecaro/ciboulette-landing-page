371632f05943437919eeeee3152465c07c05d8f3
Renan LE CARO <renan.lecaro@gmail.com>
2021-05-20 01:47:55 +0200

    blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Nouvelle fonction : modification de clients en masse","url":"https://ciboulette.net/blog/2021-05-17","date":"2021-05-17T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Déployé : corrections dans les calculs de totaux","url":"https://ciboulette.net/blog/2021-05-19","date":"2021-05-19T00:00:00.000Z"});
   
