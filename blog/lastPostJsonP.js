daf1650a927d20c9ab7e9d4aea68236b5100b625
Renan LE CARO <renan.lecaro@gmail.com>
2021-05-26 16:59:26 +0200

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Déployé : corrections dans les calculs de totaux","url":"https://ciboulette.net/blog/2021-05-19","date":"2021-05-19T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Besoin de votre avis sur la prochaine version de l'onglet \"préparation\"","url":"https://ciboulette.net/blog/2021-05-26","date":"2021-05-26T00:00:00.000Z"});
   
