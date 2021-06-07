0a2e08b0a0bf74121688dca64156909ad687636a
Renan LE CARO <renan.lecaro@gmail.com>
2021-06-07 23:53:20 +0200

    blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Besoin de votre avis sur la prochaine version de l'onglet \"préparation\"","url":"https://ciboulette.net/blog/2021-05-26","date":"2021-05-26T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Trois petits correctifs","url":"https://ciboulette.net/blog/2021-06-07","date":"2021-06-07T00:00:00.000Z"});
   
