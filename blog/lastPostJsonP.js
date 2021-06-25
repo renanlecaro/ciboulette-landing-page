e2988842f70283ae4b14d0ae5720d0040c9a1ab1
Renan LE CARO <renan.lecaro@gmail.com>
2021-06-26 00:31:41 +0200

    Blog post

--- blog/lastPostJsonP.js
+++ blog/lastPostJsonP.js
@@ -1,3 +1,3 @@
 
-  window.blogLastUpdated && window.blogLastUpdated({"title":"Trois petits correctifs","url":"https://ciboulette.net/blog/2021-06-07","date":"2021-06-07T00:00:00.000Z"});
+  window.blogLastUpdated && window.blogLastUpdated({"title":"Avancées sur le chantier de l'envoi de SMS en masse","url":"https://ciboulette.net/blog/2021-06-25","date":"2021-06-25T00:00:00.000Z"});
   
